bf321493e10df95f37f241ddec11136a52aeafa2
Anna Sedlackova <axs1202@case.edu>
2017-09-23 14:04:01 -0400

    Got LyftHandler to work correctly

--- backend/oauth.js
+++ backend/oauth.js
@@ -3,8 +3,8 @@
 */
 
 /* global configuration */
-var config = require('config.js');
-
+var expressSession = require('express-session');
+var config = require('./config');
 var lyft = require('node-lyft');
 var defaultClient = lyft.ApiClient.instance;
 
@@ -15,11 +15,11 @@ exports.handleAuthorization = function (req, res, next) {
   /* generate state value */
   var state = Date.now().toString();
   /* store state in session */
-  req.session.state = state;
+  req.state = state; //req.session.state
   /* redirect with state */
   res.redirect(
     config.LYFT_API_URI + '/oauth/authorize'
-    + '?client_id='     + config.LYFT_CLIENT_ID+"SANDBOX-" + config.LYFT_CLIENT_SECRET
+    + '?client_id='     + config.LYFT_CLIENT_ID
     + '&response_type=' + 'code'
     + '&scope='         + 'offline%20public%20profile%20rides.read%20rides.request'
     + '&state='         + state
@@ -47,27 +47,6 @@ exports.handleLanding = function (req, res, next) {
 
 exports.handleRevocation = function (req, res, next) {
   res.redirect(
-    config.LYFT_WWW_URI + '/connected-services'
+    config.LYFT_WWW_URI
   );
 };
-
-//if you're only using your Client Token for non-user specific endpoints, you can add that token directly
-//defaultClient.authentications['Client Authentication'].accessToken = 'YOUR-CLIENT-TOKEN';
-//if you're using endpoints that require a user context, you need to get your token via three-legged OAuth, then add it here:
-//defaultClient.authentications['User Authentication'].accessToken = '3-LEGGED-OAUTH-TOKEN';
-
-/*
-* Requesting a ride
-*/
-//create a new lyft-node PublicApi() instance
-var lyftPublicApi = new lyft.PublicApi()
-//the getETA endpoint works with both user and non-user context:
-//leaving the options field empty {}
-//and using promises/then to print out result
-lyftPublicApi.getETA(37.7884, -122.4076, {}).then((data) => {
-  console.log('API called successfully. Returned data: ' + data);
-  //send back to user's cellphone!
-}, (error) => {
-  console.error(error);
-  //send back to user's cellphone!
-});
