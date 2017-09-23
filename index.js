083e70e638737e1e5448e2f79b144fde0c8690ba
Vincent Portelli <vinport16@gmail.com>
2017-09-23 16:11:51 -0400

    got mongo working

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ var messagingResponse = require('twilio').twiml.MessagingResponse;
 var app = express();
 var port = process.env.port || 5757
 
+//connect database
 var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+var dburl = 'mongodb://localhost:27017';
+MongoClient.connect(dburl, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to mongo");
+  db.close();
+});
 
 http.createServer(app).listen(port, function () {
   console.log("Express server listening on port "+port);
@@ -23,7 +30,9 @@ app.get("/confirmed", function(req,res){
   }
 });
 
-app.get("/lyft", oa.handleAuthorization(req, res));
+app.get("/lyft", function(req,res){
+  oa.handleAuthorization(req, res)
+});
 
 //TODO: Make it call twilio handeler
 app.post('/sms', function(req, res) {
