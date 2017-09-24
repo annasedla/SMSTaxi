3895889f3d12f533c018642e346ea1bac189165a
Michael Smith <mes258@case.edu>
2017-09-23 22:27:44 -0400

    Redirect to our site/sms

--- backend/TwilioHandler.js
+++ backend/TwilioHandler.js
@@ -1,3 +1,13 @@
 /*
 * Handling communication between our server and twilio API
 */
+
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+const response = new MessagingResponse();
+const message = response.message();
+message.body('Hello World!');
+response.redirect('https://taxisms.azurewebsites.net/sms');
+
+console.log(response.toString());
+
